40e64f4f5f740965b5c559f6c44dc21e51d05ac6
traxcer <meloy30@hotmail.com>
2021-04-22 13:01:19 +0200

    actualiza

--- PROCESOS/procesos.c
+++ PROCESOS/procesos.c
@@ -16,10 +16,10 @@ int I=0;
 
 void codigo_del_proceso(int id){
     int i;
-    for (i=0;i<50;i++){
-        printf("Proceso %d: i= %d, I = %d\n",id, i,I);
+    for (i=0;i<50;i++)
+        printf("Proceso %d: i= %d, I = %d\n",id, i,I++);
     exit (id); //Sale del proceso con el valor de id
-    }
+    
 }
 
 int main(){
@@ -28,18 +28,17 @@ int main(){
     int pid;
     int salida;
 
-    for (p=0; p<NUM_PROCESOS;p++){
+    for (p=0; p <  NUM_PROCESOS ;p++){
         pid=fork();
         if (pid==-1){ //Error al crear el hijo
             perror("Error al crear el proceso");
             exit(-1);
-        } else if (pid==0){//Codigo porceso hijo
+        } else if (pid==0)//Codigo proceso hijo
             codigo_del_proceso(id[p]);//le pasamos el numero de proceso creado
-        } 
+    }
     //Ya esto lo ejecuta el padre. El hijo sale en la funcion llamada
     for (p=0;p<NUM_PROCESOS;p++){
         pid= wait (&salida);
         printf("Proceso %d con id = %x terminado\n", pid, salida >> 8);
-    }
     }
 }
