fb5ce6ccec44dbcb78e45b8c591dfd8a829636af
traxcer <meloy30@hotmail.com>
2021-05-11 14:34:08 +0200

    actuzalizo

--- TEMA02/sesion12_tuberias/ej1.c
+++ TEMA02/sesion12_tuberias/ej1.c
@@ -1,8 +1,4 @@
 /* 
- 
-
-
-
 El hijo, por su parte, recibe la información del padre y, análogamente, filtra el 
 contenido que le llega y escribe una copia modificada con el contenido filtrado. 
 El filtro que aplica cada proceso es el siguiente:
@@ -53,6 +49,12 @@ a|**|b
 */
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+# define T 255
+
 /*
 Escriba un programa en C que reciba como argumento una lista de ficheros de texto, 
 y que filtre su contenido de usando para ello dos procesos, padre e hijo, comunicados 
@@ -63,10 +65,12 @@ int main(int argc, char * argv[]){
         fprint("Uso: %d <listaFicheros>",argv[0]);
         exit (-1);
     }
-
     size_t pid_id;
-    
-
+    int tuberia[2];
+    if (pipe(tuberia)==-1){ //abro tuberia
+        perror("pipe: ");
+        exit (-1);
+    }
     if (pid_id=(fork())==-1){
         perror("fork");
         exit(-1);
@@ -74,7 +78,31 @@ int main(int argc, char * argv[]){
     if(pid_id==0){ // Estamos en hijo
 
     } else { // Estamos en el padre
-        fd= open(argv[1])
+    char buffer[T];
+    char filtro1[T];
+    int leidos;
+        for(int i=0;i<argc-1;i++){ //bucle que lee la lista de ficheros
+            int fd= open(argv[i],O_RDONLY);
+            if (fd==-1){
+                perror("open fichero");
+                exit(-1);
+            }
+            leidos=read(fd,buffer,T);
+            while (leidos>0){
+                //pasa a minusculas lo leido
+                for (int i=0;i<leidos+1;i++)
+                    filtro1[i]
+                buffer[leidos] = '\0';
+        printf("%s",buffer);
+        leidos =read(fd,buffer,T);
+        }
+    close(fd);
+/*      El filtro que aplica el proceso padre:
+        Convierte todas los caracteres a minúscula (ver la función tolower() 
+        en el manual)
+*/
+
+        }   
     }
 /*El padre se encarga de abrir los ficheros, aplicar el primer filtro, guardar una copia 
 modificada del fichero con el contenido filtrado y pasarlo al hijo para que aplique el 
